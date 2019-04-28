732cfabf6003d4ce33e41933fcff1206bd62b613
Naoki Kawajiri <naoki19940317@yahoo.col.jp>
2019-04-28 16:31:32 +0900

    convaで画像表示処理を書き直す

--- script/canvas-1.js
+++ script/canvas-1.js
@@ -4,79 +4,80 @@
 
 const file_btn = document.getElementById('file-btn');
 const canvas = document.getElementById('canvas');
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
+const canvasWidth = 500;
+const canvasHeight = canvas.innerHeight;
 
 // Canvasの準備
-// canvas.width = canvasWidth;
-// canvas.height = canvasHeight;
-var ctx = canvas.getContext('2d');
+// var ctx = canvas.getContext('2d');
 
+// function loadLocalImage(e) {
+//   // ファイル情報を取得
+//   const fileData = e.target.files[0];
 
-function loadLocalImage(e) {
-  // ファイル情報を取得
-  const fileData = e.target.files[0];
+//   // 画像ファイル以外は処理を止める
+//   if(!fileData.type.match('image.*')) {
+//     alert('画像を選択してください');
+//     return;
+//   }
 
-  // 画像ファイル以外は処理を止める
-  if(!fileData.type.match('image.*')) {
-    alert('画像を選択してください');
-    return;
-  }
-
-  // FileReaderオブジェクトを使ってファイル読み込み
-  const reader = new FileReader();
-  // ファイル読み込みに成功したときの処理
-  reader.onload = function() {
-    // Canvas上に表示する
-    uploadImgSrc = reader.result;
-    canvasDraw();
-  }
-  // ファイル読み込みを実行
-  reader.readAsDataURL(fileData);
-}
+//   // FileReaderオブジェクトを使ってファイル読み込み
+//   const reader = new FileReader();
+//   // ファイル読み込みに成功したときの処理
+//   reader.onload = function() {
+//     // Canvas上に表示する
+//     uploadImgSrc = reader.result;
+//     canvasDraw();
+//   };
+//   // ファイル読み込みを実行
+//   reader.readAsDataURL(fileData);
+// }
 
 // ファイルが指定された時にloadLocalImage()を実行
-file_btn.addEventListener('change', loadLocalImage, false);
+// file_btn.addEventListener('change', loadLocalImage, false);
 
 // Canvas上に画像を表示する
-function canvasDraw() {
-    // canvas内の要素をクリアする
-    // ctx.clearRect(0, 0, 100, 100);
+// function canvasDraw() {
+//     // canvas内の要素をクリアする
+//     // ctx.clearRect(0, 0, 100, 100);
+//     // Canvas上に画像を表示
+//     const img = new Image();
+//     img.src = uploadImgSrc;
+//     img.onload = function() {
+//       ctx.drawImage(img, 0, 0, 30, 30);
+//     };
+// }
 
-    // Canvas上に画像を表示
-    const img = new Image();
-    img.src = uploadImgSrc;
+window.onload.addEventListener(() => {
+    const stage = new Konva.Stage({
+        container: 'container',
+        width: canvasWidth,
+        height: canvasHeight
+    });
 
-    img.onload = function() {
-      ctx.drawImage(img, 0, 0, 30, 30);
-    }
-}
+    let imageObj = new Image();
+    imageObj.onload = () => {
+        drawImage(imageObj, stage);
+    };
+    imageObj.src = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
+});
 
 
 // イメージオブジェクトを基に描画
-function drawImage(imageObj) {
-  var stage = new Konva.Stage({
-    container: 'container',
-    width: canvasWidth,
-    height: canvasHeight
-  });
+function drawImage(imageObj, stage, option={}, layer=null) {
+    if (!layer) layer = new Konva.Layer();
 
-  var layer = new Konva.Layer();
-  // darth vader
-  var darthVaderImg = new Konva.Image({
-    image: imageObj,
-    x: stage.width() / 2 - 200 / 2,
-    y: stage.height() / 2 - 137 / 2,
-    width: 200,
-    height: 137,
-    draggable: true
-  });
+    imageObj.onload = function() {
+        const image = new Konva.Image({
+            image: imageObj,
+            x: option.x || 50,
+            y: option.y || 50,
+            width: option.width || imageObj.width,
+            height: option.height || imageObj.height,
+        });
 
-  layer.add(darthVaderImg);
-  stage.add(layer);
+        layer.add(image);
+        stage.add(layer);
+    };
+    return layer;
 }
-var imageObj = new Image();
-imageObj.onload = function() {
-  drawImage(this);
-};
-imageObj.src = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
+

6b76fad2fa496b2d295783ffcbb3b8584f43046f
Naoki Kawajiri <naoki19940317@yahoo.col.jp>
2019-04-29 11:59:51 +0900

    callbackをpromiseに変更

--- script/canvas-1.js
+++ script/canvas-1.js
@@ -4,24 +4,20 @@
 
 const canvasWidth = window.innerWidth;
 const canvasHeight = 800;
+let stage = null;
 
-window.addEventListener('load', () => {
-    const stage = new Konva.Stage({
+const windowLoadPromise = loadPromise(window);
+windowLoadPromise.then(() => {
+    stage = new Konva.Stage({
         container: 'container',
         width: canvasWidth,
         height: canvasHeight
     });
 
-    let imageObj = new Image();
-    let onloadImageLayer = null;
-    imageObj.onload = () => {
-        onloadImageLayer = drawImage(imageObj, stage, {draggable: true});
-    };
-    imageObj.src = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
-
-    // fileを選んで読み込み
+    // fileを選択して描画
     const file_btn = document.getElementById('file-btn');
-    file_btn.addEventListener('change', (event) => {
+    // file_btn.addEventListener('change', (event) => {
+    changePromise(file_btn).then((event) => {
           // ファイル情報を取得
         const fileData = event.target.files[0];
         // 画像ファイル以外は処理を止める
@@ -31,21 +27,37 @@ window.addEventListener('load', () => {
         }
         // FileReaderオブジェクトを使ってファイル読み込み
         const reader = new FileReader();
-        // ファイル読み込みに成功したときの処理
-        reader.onload = function() {
-            let selectedImageObj = new Image();
-            selectedImageObj.onload = () => {
-                drawImage(selectedImageObj, stage, {
-                    draggable: true,
-                });
-            };
+        let selectedImageObj = new Image();
+        loadPromise(reader).then(() => {
+            const promise = loadPromise(selectedImageObj);
             selectedImageObj.src = reader.result;
-        };
+            return promise;
+        }).then(() => {
+            drawImage(selectedImageObj, stage, {
+                draggable: true,
+            });
+        });
         // ファイル読み込みを実行
         reader.readAsDataURL(fileData);
     });
 });
 
+// 飛行機を描画
+const initImageObj = new Image();
+Promise.all([windowLoadPromise, loadPromise(initImageObj)]).then(() => {
+    drawImage(initImageObj, stage, {draggable: true});
+});
+initImageObj.src = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
+
+function eventPromise(obj, eventName) {
+    return new Promise(resolve => {
+        return obj.addEventListener(eventName, (e) => {
+            resolve(e);
+        });
+    });
+}
+function loadPromise(obj) {return eventPromise(obj, 'load');}
+function changePromise(obj) {return eventPromise(obj, 'change');}
 
 // イメージオブジェクトを基に描画
 function drawImage(imageObj, stage, option={}, layer=null) {
@@ -64,5 +76,9 @@ function drawImage(imageObj, stage, option={}, layer=null) {
     layer.add(image);
     stage.add(layer);
     return layer;
+}
+
+function getDistance(p1, p2) {
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 }
 
